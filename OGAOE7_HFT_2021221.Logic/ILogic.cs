eb06066e226d89ff1317d33301c4bcf54f2e1865
DanShepherd27 <jdani20001027@gmail.com>
2022-01-12 23:02:16 +0100

    ILogic interface and Logic class created

---
+++ OGAOE7_HFT_2021221.Logic/ILogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGAOE7_HFT_2021221.Logic
+{
+    interface ILogic<T> where T : class
+    {
+        // Common CRUD
+        IQueryable<T> ReadAll();
+        void Create(T newItem);
+    }
+}
---
+++ OGAOE7_HFT_2021221.Logic/Logic.cs
@@ -0,0 +1,28 @@
+﻿using OGAOE7_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGAOE7_HFT_2021221.Logic
+{
+    class Logic<T> : ILogic<T> where T: class
+    {
+        protected IRepository<T> repo;
+        public Logic(IRepository<T> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(T newItem)
+        {
+            repo.Create(newItem);
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+    }
+}
