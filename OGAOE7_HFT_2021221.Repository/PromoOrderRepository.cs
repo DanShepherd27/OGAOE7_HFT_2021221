d2580a844c3d24c69bfee5136239ceb6b6d4cf9b
DanShepherd27 <jdani20001027@gmail.com>
2022-01-12 22:54:15 +0100

    PromoRepository created, CRUD methods added

---
+++ OGAOE7_HFT_2021221.Repository/PromoOrderRepository.cs
@@ -0,0 +1,67 @@
+﻿using OGAOE7_HFT_2021221.Data;
+using OGAOE7_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGAOE7_HFT_2021221.Repository
+{
+    public class PromoOrderRepository : Repository<PromoOrder>, IPromoOrderRepository
+    {
+        public PromoOrderRepository(PizzaContext ctx) : base(ctx)
+        {
+            /* ... */
+        }
+
+        #region CRUD
+        public override void Create(PromoOrder promoOrder)
+        {
+            ctx.Orders.Add(promoOrder);
+            ctx.SaveChanges();
+        }
+        public PromoOrder Read(int id)
+        {
+            try
+            {
+                return ctx.Orders.SingleOrDefault(x => x.Id == id);
+            }
+            catch(ArgumentNullException)
+            {
+                return default(PromoOrder);
+            }
+            catch(InvalidOperationException)
+            {
+                return default(PromoOrder);
+            }
+        }
+
+        public void Update(PromoOrder promoOrder)
+        {
+            PromoOrder oldOrder = Read(promoOrder.Id);
+            oldOrder.DiscountPercentage = promoOrder.DiscountPercentage;
+            oldOrder.DrinkName = promoOrder.DrinkName;
+            oldOrder.PizzaName = promoOrder.PizzaName;
+            oldOrder.TimeOfOrder = promoOrder.TimeOfOrder;
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Orders.Remove(Read(id));
+            ctx.SaveChanges();
+        }
+        #endregion
+
+        #region NON-CRUD
+        public void ChangeDiscountPercentage(int id, int newPercentage)
+        {
+            var order = Read(id);
+            order.DiscountPercentage = newPercentage;
+            ctx.SaveChanges();
+        }
+        #endregion
+    }
+
+}
