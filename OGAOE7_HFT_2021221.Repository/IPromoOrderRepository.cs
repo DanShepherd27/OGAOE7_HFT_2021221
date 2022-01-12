232d564192ae0a7b9291fed18328cfe6f46a4afc
DanShepherd27 <jdani20001027@gmail.com>
2022-01-12 22:54:01 +0100

    IPromoRepository created

---
+++ OGAOE7_HFT_2021221.Repository/IPromoOrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using OGAOE7_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGAOE7_HFT_2021221.Repository
+{
+    public interface IPromoOrderRepository : IRepository<PromoOrder>
+    {
+        #region CRUD
+        PromoOrder Read(int id);
+        void Update(PromoOrder promoOrder);
+        void Delete(int id);
+        #endregion
+
+        #region NON-CRUD
+        void ChangeDiscountPercentage(int id, int newPercentage);
+        #endregion
+    }
+}
