d15b10be8cf3fcc62f0b020f4e77e701878cb296
DanShepherd27 <jdani20001027@gmail.com>
2022-01-12 22:33:51 +0100

    Repository base class and IRepository base interface created

---
+++ OGAOE7_HFT_2021221.Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using OGAOE7_HFT_2021221.Models;
+
+namespace OGAOE7_HFT_2021221.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        // Common CRUD
+        IQueryable<T> ReadAll();
+        void Create(T newItem);
+    }
+}
---
+++ OGAOE7_HFT_2021221.Repository/Repository.cs
@@ -0,0 +1,26 @@
+﻿using OGAOE7_HFT_2021221.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGAOE7_HFT_2021221.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected PizzaContext ctx;
+
+        public Repository(PizzaContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public abstract void Create(T newItem);
+
+        public IQueryable<T> ReadAll()
+        {
+            return this.ctx.Set<T>(); //"Set" means dataset here, not setter
+        }
+    }
+}
