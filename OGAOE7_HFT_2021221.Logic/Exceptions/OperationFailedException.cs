8c55f6938dbff8867d0e58e6e732cbc57faa30c5
DanShepherd27 <jdani20001027@gmail.com>
2022-01-21 14:58:01 +0100

    Two custom exceptions created

---
+++ OGAOE7_HFT_2021221.Logic/Exceptions/OperationFailedException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGAOE7_HFT_2021221.Logic.Exceptions
+{
+    public class OperationFailedException<T> : Exception
+    {
+        Action<T> failedMethod;
+        public OperationFailedException() : base("Performing operation has failed.")
+        {
+
+        }
+
+        public OperationFailedException(string message) : base(message)
+        {
+        
+        }
+        public OperationFailedException(string message, Action<T> failedMethod) : base(message)
+        {
+            this.failedMethod = failedMethod;
+        }
+        public OperationFailedException(Action<T> failedMethod) : base($"Performing operation {failedMethod.Method} has failed.")
+        {
+            this.failedMethod = failedMethod;
+        }
+    }
+}
---
+++ OGAOE7_HFT_2021221.Logic/Exceptions/UnsupportedValueException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGAOE7_HFT_2021221.Logic.Exceptions
+{
+    public class UnsupportedValueException : Exception
+    {
+        public int Id { get; set; }
+        public UnsupportedValueException(int id) : base($"Value is not supported: {id} Don't be stoopid!")
+        {
+            Id = id;
+        }
+    }
+}
