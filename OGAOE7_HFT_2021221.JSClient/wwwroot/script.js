f577ccc6fb17a51e40fbdbba78ccc398bac35591
DanShepherd27 <jdani20001027@gmail.com>
2022-05-07 18:36:44 +0200

    Update function

--- OGAOE7_HFT_2021221.JSClient/wwwroot/script.js
+++ OGAOE7_HFT_2021221.JSClient/wwwroot/script.js
@@ -1,13 +1,54 @@
 ﻿let pizzas = [];
+let connection = null;
+let pizzaIdToUpdate = -1;
 
 getdata();
+setupSignalR();
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:26548/hub")
+        .configureLogging(signalR.LogLevel.information)
+        .build();
+
+    connection.on("PizzaCreated", (user, message) => {
+        //console.log(user);
+        //console.log(message);
+        getdata();
+    });
+
+    connection.on("PizzaDeleted", (user, message) => {
+        //console.log(user);
+        //console.log(message);
+        getdata();
+    });
+
+    connection.on("PizzaUpdated", (user, message) => {
+        getdata();
+    });
+
+    connection.onclose(async () => {
+        await start();
+    });
+    start();
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+}
 
 async function getdata() {
     await fetch('http://localhost:26548/pizza')
         .then(x => x.json())
         .then(y => {
             pizzas = y;
-            console.log(pizzas);
+            //console.log(pizzas);
             display();
         });
 }
@@ -22,11 +63,26 @@ function display() {
                 <td> ${t.name} </td>
                 <td> ${t.price} </td>
                 <td> ${t.promotional} </td>
-                <td>
-                    <button type="button" onclick="remove(${t.id})">Delete</button>
+                <td id="buttoncolumn">
+                    <button type="button" class="deletebutton" onclick="remove(${t.id})">Delete</button>
+                    <button type="button" class="updatebutton" onclick="showupdate(${t.id}); hideadd();">Update</button>
                 </td>
             </tr>`;
     });
+}
+
+function create() {
+    let name = document.getElementById('pizzaname').value;
+    let price = document.getElementById('pizzaprice').value;
+    fetch('http://localhost:26548/pizza', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify({ name: name, price: price }),
+    })
+        .then(response => response)
+        .then(data => { console.log('Success: ', data); getdata(); })
+        .catch((error) => { console.error('Error: ', error); });
+    getdata();
 }
 
 function remove(id) {
@@ -43,16 +99,41 @@ function remove(id) {
         .catch((error) => { console.error('Error: ', error); })
 }
 
-function create() {
-    let name = document.getElementById('pizzaname').value;
-    let price = document.getElementById('pizzaprice').value;
+function update() {
+    let name = document.getElementById('pizzanametoupdate').value;
+    let price = document.getElementById('pizzapricetoupdate').value;
+    let pizza = { id: pizzaIdToUpdate, name: name, price: price };
+    console.log(pizza);
     fetch('http://localhost:26548/pizza', {
-        method: 'POST',
+        method: 'PUT',
         headers: { 'Content-Type': 'application/json', },
-        body: JSON.stringify({ name: name, price: price }),
+        body: JSON.stringify({ id: pizzaIdToUpdate, name: name, price: price }),
     })
         .then(response => response)
-        .then(data => { console.log('Success: ', data); getdata(); })
+        .then(data => {
+            console.log('Success: ', data);
+            getdata();
+        })
         .catch((error) => { console.error('Error: ', error); });
-    getdata();
+    hideupdate();
+    showadd();
+}
+
+function showadd() {
+    document.getElementById('formdiv').style.display = 'flex';
+}
+
+function hideadd() {
+    document.getElementById('formdiv').style.display = 'none';
+}
+
+function showupdate(id) {
+    document.getElementById('pizzanametoupdate').value = pizzas.find(t => t['id'] == id)['name'];
+    document.getElementById('pizzapricetoupdate').value = pizzas.find(t => t['id'] == id)['price'];
+    document.getElementById('updateformdiv').style.display = 'flex';
+    pizzaIdToUpdate = id;
+}
+
+function hideupdate() {
+    document.getElementById('updateformdiv').style.display = 'none';
 }
